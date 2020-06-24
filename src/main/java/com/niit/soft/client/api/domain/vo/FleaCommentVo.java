d1046630c4227e2e53eb338d1d4611d3a5e4b616
郭瑞昌 <48038411@qq.com>
2020-06-24 13:47:26 +0800

    在所有实体类添加了注解，修复了时区差8的问题

--- src/main/java/com/niit/soft/client/api/domain/vo/FleaCommentVo.java
+++ src/main/java/com/niit/soft/client/api/domain/vo/FleaCommentVo.java
@@ -1,5 +1,6 @@
 package com.niit.soft.client.api.domain.vo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -37,6 +38,7 @@ public class FleaCommentVo {
      */
     private String title;
 
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
     private Date createTime;
 
     private String commentAvatar;
--- src/main/java/com/niit/soft/client/api/domain/vo/GoodsVo.java
+++ src/main/java/com/niit/soft/client/api/domain/vo/GoodsVo.java
@@ -1,5 +1,6 @@
 package com.niit.soft.client.api.domain.vo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -24,6 +25,7 @@ public class GoodsVo {
     private String goodsDescription;
     private String goodsMarks;
     private String goodsImgUrl;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
     private Date goodsCreateTime;
     private String goodsType;
     private String goodsUserName;
